589b87f3d9d67e2b397cee7d1ce4c3d15ff6ec6f
Jawahar003 <35863321+Jawahar003@users.noreply.github.com>
2019-02-06 14:52:47 +0530

    Create noodigits.c

---
+++ noodigits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    int count=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        n/= 10;
+        ++count;
+    }
+    printf("%d",count);
+}
